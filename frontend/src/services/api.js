8c5551dbe21c920dbe8299ab08bde43c9ed55092
sujal <saisujaldevendran@gmail.com>
2026-02-17 14:45:08 +0530

    api commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 const BASE_URL = "http://127.0.0.1:8000/api"
 
 export const fetchUsers = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/users/")
+  const res = await fetch(`${BASE_URL}/users/`)
   return res.json()
 }
 
@@ -11,20 +11,20 @@ export const fetchSummary = async () => {
 }
 
 export const fetchInsights = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/insights/")
+  const res = await fetch(`${BASE_URL}/insights/`)
   return res.json()
 }
 export const fetchModelMetrics = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/model-metrics/")
+  const res = await fetch(`${BASE_URL}/model-metrics/`)
   return res.json()
 }
 
 export const fetchConfusionMatrix = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/confusion-matrix/")
+  const res = await fetch(`${BASE_URL}/confusion-matrix/`)
   return res.json()
 }
 export const retrainModel = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/train-model/", {
+  const res = await fetch(`${BASE_URL}/train-model/`, {
     method: "POST"
   })
   return res.json()
